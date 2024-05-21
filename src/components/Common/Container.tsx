f592e76d0e154d7a38f7ed804c01d0ace2791a87
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-05-21 20:57:14 +0900

    chore: 반응형 수정

--- src/components/Common/Container.tsx
+++ src/components/Common/Container.tsx
@@ -20,7 +20,11 @@ const Container: FC<{ children: ReactNode }> = ({ children }) => {
   }, []);
 
   return (
-    <div className="relative container-shadow shadow-2xl bg-white w-full sm:max-w-[26.875rem] h-screen 2xl:h-[58.25rem] overflow-y-scroll overflow-x-hidden left-0 mx-auto translate-x-0 lg:left-1/2 lg:mx-0 lg:translate-x-[50px] no-scroll-bar">
+    <div
+      className="relative container-shadow shadow-2xl bg-white w-full overflow-y-scroll overflow-x-hidden no-scroll-bar 
+   h-screen h-md:max-h-[58.25rem] max-w-[26.875rem] 
+    xs:max-w-[26.875rem] left-0 mx-auto translate-x-0 lg:left-1/2 lg:mx-0 xl:translate-x-[6rem] 2xl:translate-x-[15rem]"
+    >
       {children}
     </div>
   );
