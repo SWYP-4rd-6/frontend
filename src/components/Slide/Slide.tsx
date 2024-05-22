e992906ff8b2355f83a096c0fcf3427102dfa3ed
jew2222 <72792459+jew2222@users.noreply.github.com>
2024-05-23 07:22:03 +0900

    chore: 이미지 비율 변경

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -5,14 +5,15 @@ type PropsType = {
   onClick?: () => void;
 };
 
+/* */
 const Slide = ({ onClick, src }: PropsType) => {
   return (
-    <div className=" h-[27rem] relative " onClick={onClick}>
+    <div className=" h-[27rem] relative cursor-pointer" onClick={onClick}>
       <div
-        className="inset-0 size-full bg-no-repeat bg-cover bg-center "
+        className="inset-0 size-full bg-cover bg-center bg-no-repeat "
         style={{ backgroundImage: `url(${src})` }}
       ></div>
-      <div className="absolute inset-0 bg-[#0173FA] bg-opacity-20"></div>
+      <div className="blue-filter"></div>
     </div>
   );
 };
