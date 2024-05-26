a8113b3f5e3552b18ee59c79a6ab3140f1e7a956
jew2222 <jewallk@gmail.com>
2024-05-26 17:55:56 +0900

    chore : 로딩 화면 스타일 변경

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="w-16 h-16 border-4 border-t-4 border-t-blue-500 border-gray-200 rounded-full animate-spin"></div>
+    <div className="fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center z-40">
+      <div className="size-10 border-8 border-t-8 border-t-blue-500 border-gray-200 rounded-full animate-spin"></div>
+      <p className="mx-4 animate-pulse">로딩중 입니다...</p>
     </div>
   );
 };
