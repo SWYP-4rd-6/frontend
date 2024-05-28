444ec89a9449229cc82289567036893104054b74
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-05-29 00:48:20 +0900

    chore: 로딩화면개선

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="w-16 h-16 border-4 border-t-4 border-t-blue-500 border-gray-200 rounded-full animate-spin"></div>
+    <div className="fixed inset-0 bg-white bg-opacity-50 flex flex-col items-center justify-center z-50">
+      <div className="w-16 h-16 border-4 border-t-4 border-t-blue-500 border-gray-200 rounded-full animate-spin mb-4"></div>
+      <div>Loading ...</div>
     </div>
   );
 };
